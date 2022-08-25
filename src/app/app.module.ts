71fe8f43f5bce22c9bd4c8cc523f1f86a808b514
Agrawal <pankajagrawal2@deloitte.com>
2022-08-25 18:55:14 +0530

    some ui code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { AngularMaterialModule } from './material-module/material.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FileUploadComponent } from './components/file-upload/file-upload.component';
+import { TableComponent } from './components/table/table.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FileUploadComponent
+    FileUploadComponent,
+    TableComponent
   ],
   imports: [
+    AngularMaterialModule,
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
