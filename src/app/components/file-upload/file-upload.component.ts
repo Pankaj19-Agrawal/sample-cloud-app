6fb240d33834a0765c9b47a8e0a5c1f7d0927b7e
Agrawal <pankajagrawal2@deloitte.com>
2022-09-01 22:16:37 +0530

    code optimization

--- src/app/components/file-upload/file-upload.component.ts
+++ src/app/components/file-upload/file-upload.component.ts
@@ -14,43 +14,74 @@ export class FileUploadComponent {
 	@Output() jsonData: EventEmitter<IfileContentJson[]> = new EventEmitter<IfileContentJson[]>();
 	uploadButton: string = MessageConstant.UPLOAD_BUTTON;
 	file = null;
-
+	tableData:IfileContentJson[]
 	selectedFiles: any;
 	currentFileUpload: FileUpload;
 	percentage: number;
 
 	constructor(private fileUploadService: FileUploadService, private commonService: CommonService) { }
 
+	//on file change
 	onChange(event: any) {
-		this.file = event.target;
-		// this.selectedFiles = event.target.files;  
-		this.xyz();
+		this.file = event.target;  
+		this.loadPlainFile();
 	}
 
-	getTextareaValue(){
-		let value:any = document.querySelector('textarea')?.value;
-		console.log(value);
-		
-		
-		
-		var plainText = value
-		var rtf = this.convertToRtf(plainText);
-		var plainText2 = this.convertToPlain(rtf);
-		// console.log('rtf 1',rtf)
-		// console.log('rtf 2',plainText2)
-
-
-		this.abc(plainText2)
+	//on file upload
+	onUpload() {
+		this.setTableData();
+		this.getPlainFileContent();
 	}
 
-	abc(value:any){
-		var div = document.getElementById('mytext') as HTMLInputElement;
-	    // var text = '<span style="background:red">hello</span>'
-	    div.innerHTML = value
-		this.showHighlightedDocument(div);
+	//pass data to table component
+	setTableData() {
+		this.tableData = [
+			{
+				category: 'Automobile',
+				value: 'a usually four-wheeled vehicle designed primarily for passenger transportation'
+			},
+			{
+				category: 'Computer-Science',
+				value: 'Unlike electrical and computer engineers, computer scientists deal mostly with software and software systems'
+			}
+		];
 	}
 
-	showHighlightedDocument(doc: any) {
+	//set uploaded file data in textarea
+	loadPlainFile(){
+		let input = document.querySelector('input');
+		let textArea:any = document.querySelector('textarea');
+
+		let files:any = input?.files;
+		const file = files[0];
+		if(files?.length == 0) return;
+
+		let reader = new FileReader();
+		reader.onload = (e) =>{
+			const file:any = e.target?.result;
+			const lines = file.split(/\r\n|\n/);
+			textArea.value = lines.join('\n');
+		};
+
+		reader.onerror = (e) => alert(e.target?.error?.name);
+		reader.readAsText(file);
+	}
+
+	//get textarea content
+	getPlainFileContent(){
+		let content:any = document.querySelector('textarea')?.value;
+		this.setPreElementContent(content);
+	}
+
+	//set content for pre element
+	setPreElementContent(content:any){
+		let preElement = document.getElementById('pre-element') as HTMLInputElement;
+	    preElement.innerHTML = content;
+		this.convertPlainTextToHighlightedText(preElement);
+	}
+
+	//set highlighted text into pre element
+	convertPlainTextToHighlightedText(preElement: any) {
 		let contentJson = [
 			{
 				category: 'Automobile',
@@ -62,39 +93,11 @@ export class FileUploadComponent {
 			}
 		];
 		contentJson.forEach((item: IfileContentJson) => {
-		  doc.innerHTML = doc.innerHTML.replace(item.value, `<span style="background:yellow" id="{{item.category}}">${item.value}</span>`)
+		  preElement.innerHTML = preElement.innerHTML.replace(item.value, '<span style="background:yellow" id="'+item.category+'" title="'+item.category+'">'+item.value+'</span>')
 		});
 	}
 
-	xyz(){
-		let input = document.querySelector('input');
-		let output:any = document.querySelector('textarea');
-		let output2:any = document.getElementById('mytext') as HTMLInputElement;
-
-		let files:any = input?.files;
-		const file = files[0];
-		if(files?.length == 0) return;
-
-		let reader = new FileReader();
-		reader.onload = (e) =>{
-			const file:any = e.target?.result;
-			const lines = file.split(/\r\n|\n/);
-			output.value = lines.join('\n');
-			output2.innerHTML = lines.join('\n');
-		};
-
-		reader.onerror = (e) => alert(e.target?.error?.name);
-		reader.readAsText(file);
-
-	}
-
-	onUpload() {
-		// this.fileUploadService.upload(this.file).subscribe((res: IfileContentJson) => {
-		// 	if (typeof (res) === 'object') {}
-		// });
-
-		this.getJsonOutput()
-	}
+	
 
 	// onUpload() {
 	// 	const file = this.selectedFiles.item(0);
@@ -110,41 +113,4 @@ export class FileUploadComponent {
 	// 	);
 	// }
 
-	getJsonOutput() {
-		let fileContentJson = [
-			{
-				category: 'Automobile',
-				value: 'a usually four-wheeled vehicle designed primarily for passenger transportation'
-			},
-			{
-				category: 'Computer-Science',
-				value: 'Unlike electrical and computer engineers, computer scientists deal mostly with software and software systems'
-			}
-		];
-		this.jsonData.emit(fileContentJson);
-	}
-
-	
-
-convertToRtf(plain:any) {
-    plain = plain.replace(/\n/g, "\\par\n");
-    return "{\\rtf1\\ansi\\ansicpg1252\\deff0\\deflang2057{\\fonttbl{\\f0\\fnil\\fcharset0 Microsoft Sans Serif;}}\n\\viewkind4\\uc1\\pard\\f0\\fs17 " + plain + "\\par\n}";
 }
-
-convertToPlain(rtf:any) {
-    rtf = rtf.replace(/\\par[d]?/g, "");
-    return rtf.replace(/\{\*?\\[^{}]+}|[{}]|\\\n?[A-Za-z]+\n?(?:-?\d+)?[ ]?/g, "").trim();
-}
-
-}
-
-
-// a(event:any){
-// 	var fr=new FileReader();
-// 		fr.onload=function(){
-// 			let ab:any = document.getElementById('output') as HTMLInputElement
-// 					ab.innerHTML = fr.result;
-// 		}
-		  
-// 		fr.readAsText(event.target.files[0])
-// }
